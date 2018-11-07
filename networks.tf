16b0e9f70bb5a1456c51df9dc21e279785b14458
Radek Zika <13420016+zikarad@users.noreply.github.com>
2018-11-07 16:46:50 +0100

    Tags and vpc options improved

--- networks.tf
+++ networks.tf
@@ -2,10 +2,12 @@
 resource "aws_vpc" "vpc-main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags {
-    Name  = "${var.prefix}"
-    stage = "${var.stage}"
+    Name    = "${var.prefix}"
+    stage   = "${var.stage}"
+    creator = "Terraform"
   }
 }
 
