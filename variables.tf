27050574a63ffd29391f263d8aa5e07597f699e0
zikarad <13420016+zikarad@users.noreply.github.com>
2019-02-16 16:06:12 +0100

    removed K8s stuff, added retention variable

--- variables.tf
+++ variables.tf
@@ -1,16 +1,11 @@
 variable "domain_name" { default = "radekzika.cloud" }
 
 variable "prefix" { default = "single-az" }
-variable "stage" { default = "poc"}
+variable "stage"  { default = "poc"}
 
 variable "subnets" {
     type = "list"
     default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "clname" {
-  description = "Kubernetes Test Cluster"
-  default = "ktc"
 }
 
 /* AWS */
@@ -22,18 +17,19 @@ variable "ami" {
 variable "region" { default = "eu-central-1" }
 variable "zone"   { default = "a" }
 
-variable "sshkey_path" {}
+variable "sshkey_path"    {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "route53zone" {}
+variable "route53zone"    {}
 variable "s3-bucket-name" {
   description = "Common bucket for the project"
   default     = "single-az-project"
 }
 
-variable "host-size"  { default = "t3.medium" }
-variable "spot-price" { default = "0.03" }
+variable "host-size"       { default = "t3.medium" }
+variable "spot-price"      { default = "0.03" }
+variable "cwlog-retention" { default = "7" }
 
-variable "hostcount" { default = 1 }
+variable "hostcount"   { default = 1 }
 
 variable "sshkey_name" { default = "aws_gen" }
