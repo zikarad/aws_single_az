c70f11ee823e277bfd84c64fa8944faabcecf64e
zikarad <13420016+zikarad@users.noreply.github.com>
2019-02-18 14:33:02 +0100

    vpc cidr range moved to variables

--- networks.tf
+++ networks.tf
@@ -1,6 +1,6 @@
 /* --- VPCs */
 resource "aws_vpc" "vpc-main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "${var.vpc_cidr}"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
--- variables.tf
+++ variables.tf
@@ -3,6 +3,8 @@ variable "domain_name" { default = "radekzika.cloud" }
 variable "prefix" { default = "single-az" }
 variable "stage"  { default = "poc"}
 
+variable "vpc_cidr" { default = "10.0.0.0/16"}
+
 variable "subnets" {
     type = "list"
     default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
