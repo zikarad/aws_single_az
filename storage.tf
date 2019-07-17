dab1e328e5359b1c1de349d0e547a4ac4e8e165b
zikarad <13420016+zikarad@users.noreply.github.com>
2019-07-17 07:42:22 +0200

    added: conditional bucket, cloudwatch

--- storage.tf
+++ storage.tf
@@ -1,4 +1,7 @@
 data "template_file" "policy-tpl" {
+
+  count  = "${var.s3-install ? 1 : 0 }"
+
   template = "${file("s3-policy.tpl")}"
 
   vars     = {
@@ -7,6 +10,9 @@ data "template_file" "policy-tpl" {
 }
 
 resource "aws_iam_policy" "s3-policy" {
+
+  count  = "${var.s3-install ? 1 : 0 }"
+
   name    = "s3-bucket-rw"
   path    = "/"
   description = "Policy for read/write operation on bucket dedicated to host"
@@ -16,11 +22,16 @@ resource "aws_iam_policy" "s3-policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ec2-s3-single_bucket-attach" {
+
+  count  = "${var.s3-install ? 1 : 0 }"
+
   role       = "${aws_iam_role.iamr-ec2.name}"
   policy_arn = "${aws_iam_policy.s3-policy.arn}"
 }
 
 resource "aws_s3_bucket" "s3-bucket" {
+  count  = "${var.s3-install ? 1 : 0 }"
+
   bucket = "${var.s3-bucket-name}"
 
   acl    = "private"
