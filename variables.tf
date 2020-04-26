85520cffdf046138e7523565d73ef71214dc5e5f
zikarad <13420016+zikarad@users.noreply.github.com>
2020-04-26 22:23:48 +0200

    Cloudwatch installed conditionally

--- variables.tf
+++ variables.tf
@@ -23,6 +23,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "route53zone"    {}
 variable "s3-install" { default = "0" }
+variable "cw-install" { default = "0" }
 variable "s3-bucket-name" {
   description = "Common bucket for the project"
   default     = "single-az-project"
